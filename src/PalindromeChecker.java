5cdd05cf26a02824112d35c5a6ac8e9976dd3b39
Chris Bay <chris@chrisbay.net>
2018-04-18 10:05:07 -0500

    Adds empty classes

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Chris Bay
+ */
+public class PalindromeChecker {
+
+}
---
+++ src/PalindromeCheckerTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Chris Bay
+ */
+public class PalindromeCheckerTest {
+
+}
